031245497fb2d5ac557d4a5a9be0a55aaf2beb3b
Trinity <trinperkins@gmail.com>
2020-06-08 20:30:08 +0000

    games

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,11 +37,11 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(350, 400);
-        createPlatform(250, 300);
+        createPlatform(350, 400
+        createPlatform(250, 300, 0.9);
         
         platform.create(100, 580);
-        platform.create(100, 350);
+        platform.create(100, 350, 0.9);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
